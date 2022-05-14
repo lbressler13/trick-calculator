5a3ca40326c419d8017e762079aa129fd69f12d1
lbressler13 <lbressler13@gmail.com>
2022-05-14 13:13:21 -0400

    UI updates (#22)
    * feat: initial border span, very ugly
    
    * feat: dev mode switch
    
    * minor updates to border + default dev mode on
    
    * fix: correct padding around box
    
    * feat: initial multi-line box
    
    * feat: top, bottom, and middle border spans (separate)
    
    * chore: minor change + remove unused code
    
    * feat: settings icon in dev mode
    
    * refactor: shared fn to init settings dialog
    
    * feat: handle dark mode event
    
    * feat: setting to show/hide settings button
    
    * fix: dev mode switch works on all pages
    
    * fix: clear box on CE and fix initial dev mode setting
    
    * refactor: use paths in border spans
    
    * feat: unused custom view
    
    * docs: document new setting

--- app/src/main/java/com/example/trickcalculator/MainActivity.kt
+++ app/src/main/java/com/example/trickcalculator/MainActivity.kt
@@ -6,18 +6,23 @@ import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
+import androidx.appcompat.widget.SwitchCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.trickcalculator.databinding.ActivityMainBinding
 import com.example.trickcalculator.ext.disable
 import com.example.trickcalculator.ext.enable
 import com.example.trickcalculator.ext.gone
 import com.example.trickcalculator.ext.visible
 import com.example.trickcalculator.ui.main.MainFragment
+import com.example.trickcalculator.ui.shared.SharedViewModel
 
 /**
  * Activity that contains all functionality of app
  */
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private var isDarkMode = true
+    private lateinit var viewModel: SharedViewModel
 
     /**
      * Initialize activity
@@ -27,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         assignTheme()
         binding = ActivityMainBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this)[SharedViewModel::class.java]
 
         setContentView(binding.root)
         initDevModeSwitch()
@@ -38,15 +44,34 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        val wasDarkMode = isDarkMode
+        assignTheme()
+        if (wasDarkMode != isDarkMode) {
+            recreate()
+        }
+    }
+
     /**
      * Assign theme based on night mode
      */
     private fun assignTheme() {
         val nightModeFlags: Int = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
         when (nightModeFlags) {
-            Configuration.UI_MODE_NIGHT_YES -> setTheme(R.style.CustomDark)
-            Configuration.UI_MODE_NIGHT_NO -> setTheme(R.style.CustomLight)
-            else -> setTheme(R.style.CustomDark)
+            Configuration.UI_MODE_NIGHT_YES -> {
+                setTheme(R.style.CustomDark)
+                isDarkMode = true
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                setTheme(R.style.CustomLight)
+                isDarkMode = false
+            }
+            else -> {
+                setTheme(R.style.CustomDark)
+                isDarkMode = true
+            }
         }
    }
 
@@ -54,7 +79,7 @@ class MainActivity : AppCompatActivity() {
      * Initialize dev mode switch based on build type and theme
      */
     private fun initDevModeSwitch() {
-        val switch = binding.actionBar.devModeSwitch
+        val switch: SwitchCompat = binding.actionBar.devModeSwitch
 
         if (BuildOptions.buildType == "dev") {
             val checkedColor = TypedValue()
@@ -75,9 +100,14 @@ class MainActivity : AppCompatActivity() {
             )
             switch.trackDrawable.setTintList(buttonStates)
 
+            switch.isChecked = true
+            viewModel.setIsDevMode(true)
+            switch.setOnCheckedChangeListener { _, isChecked -> viewModel.setIsDevMode(isChecked) }
+
             switch.enable()
             switch.visible()
         } else {
+            switch.isChecked = false
             switch.disable()
             switch.gone()
         }
