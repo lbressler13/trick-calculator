4a45f2617d229d392b4296a386c4173daf3cfa40
lbressler13 <lbressler13@gmail.com>
2022-10-13 00:20:28 -0400

    chore: ktlint

--- app/src/espresso/java/xyz/lbres/trickcalculator/helpers/viewaction/ActionOnNestedItemViewAtPositionViewAction.kt
+++ app/src/espresso/java/xyz/lbres/trickcalculator/helpers/viewaction/ActionOnNestedItemViewAtPositionViewAction.kt
@@ -6,14 +6,12 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.PerformException
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
-import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.util.HumanReadables
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
 import xyz.lbres.kotlinutils.generic.ext.ifNull
-import xyz.lbres.trickcalculator.ui.attributions.authorattribution.AuthorAttributionViewHolder
 
 /**
  * [ViewAction] similar to [ActionOnItemViewAtPositionViewAction], using nested RecyclerViews.
