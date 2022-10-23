efdedd88ffa89d71eac58625ea040267a7e8db28
lbressler13 <lbressler13@gmail.com>
2022-10-22 21:43:17 -0400

    docs: add comments to retry rule

--- app/src/espresso/java/xyz/lbres/trickcalculator/testutils/rules/RetryRule.kt
+++ app/src/espresso/java/xyz/lbres/trickcalculator/testutils/rules/RetryRule.kt
@@ -6,7 +6,7 @@ import org.junit.runners.model.Statement
 import xyz.lbres.trickcalculator.utils.AppLogger
 
 /**
- * [TestRule] to re-run a failed test, to reduce failures in flaky tests.
+ * [TestRule] to re-run a failed test, in order to reduce failures in flaky tests.
  *
  * Adapted from similar rule defined by Andres Sandoval in this article:
  * https://andresand.medium.com/retry-testrule-for-android-espresso-tests-74683ee3b845
@@ -15,11 +15,28 @@ import xyz.lbres.trickcalculator.utils.AppLogger
  * Defaults to 2.
  */
 class RetryRule(val maxRetries: Int = 2) : TestRule {
+    /**
+     * Apply rule to a test with a given description
+     *
+     * @param test [Statement]: test to run
+     * @param description [Description]: description of test
+     * @return [Statement]
+     */
     override fun apply(test: Statement, description: Description) = runTest(test, description)
 
+    /**
+     * Get a statement to run and retry a test
+     *
+     * @param test [Statement]: test to run
+     * @param description [Description]: description of test
+     * @return [Statement]
+     */
     private fun runTest(test: Statement, description: Description): Statement {
         return object : Statement() {
-            @Throws(Throwable::class)
+            /**
+             * Run the test, and retry on failure based on [maxRetries]
+             * @throws [Throwable] if test fails on all retries
+             */
             override fun evaluate() {
                 var error: Throwable? = null
 
