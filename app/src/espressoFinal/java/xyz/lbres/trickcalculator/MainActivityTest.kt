f16810865b6364c4339cc55afe535c0e02d6528b
lbressler13 <lbressler13@gmail.com>
2022-10-24 19:40:41 -0400

    fix: imports in final flavor tests

--- app/src/espressoFinal/java/xyz/lbres/trickcalculator/MainActivityTest.kt
+++ app/src/espressoFinal/java/xyz/lbres/trickcalculator/MainActivityTest.kt
@@ -3,8 +3,10 @@ package xyz.lbres.trickcalculator
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import xyz.lbres.trickcalculator.testutils.rules.RetryRule
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
--- app/src/espressoFinal/java/xyz/lbres/trickcalculator/ProductFlavorTest.kt
+++ app/src/espressoFinal/java/xyz/lbres/trickcalculator/ProductFlavorTest.kt
@@ -7,7 +7,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import xyz.lbres.trickcalculator.testutils.isNotPresented
+import xyz.lbres.trickcalculator.testutils.rules.RetryRule
+import xyz.lbres.trickcalculator.testutils.viewassertions.isNotPresented
 
 @RunWith(AndroidJUnit4::class)
 class ProductFlavorTest {
